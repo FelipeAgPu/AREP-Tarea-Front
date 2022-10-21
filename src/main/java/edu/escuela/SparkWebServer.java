31471d4e825e1cadc4432a2cb91757f80901c53e
Juan Felipe Aguas Pulido <juan.aguas@mail.escuelaing.edu.co>
2022-10-20 19:54:22 -0500

    Finished Front in AWS

--- src/main/java/edu/escuela/SparkWebServer.java
+++ src/main/java/edu/escuela/SparkWebServer.java
@@ -1,14 +1,11 @@
 package edu.escuela;
 
-import org.apache.http.client.methods.HttpPost;
-
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 import static spark.Spark.*;
 
@@ -17,7 +14,44 @@ public class SparkWebServer {
     public static void main(String... args){
         port(getPort());
         staticFiles.location("/public");
-        post("log", (request, response) -> "Hola");
+        post("log", (request, response) -> roundRobin(request.body()));
+    }
+
+    public static int server = 0;
+
+    public static String roundRobin(String request) throws IOException {
+        if (server > 2){
+            server = 0;
+        }
+        String url = "http://ec2-3-88-222-210.compute-1.amazonaws.com:3500"+server+"/save";
+        server++;
+        System.out.println("conectando a "+ url);
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "text/plain");
+        con.setRequestProperty("Accept", "text/plain");
+        con.setDoOutput(true);
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] input = request.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
+            return response.toString();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return null;
     }
 
     private static int getPort() {
